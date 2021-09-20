7103146bc3ad75e1a36f11caa10d16cecbc4c854
Alexandre Elles <alexandre.elles@ts.fujitsu.com>
2021-09-20 12:21:59 +0200

    premier button

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {Button} from "./Component/UI/Button/Button";
+
+function App() {
+  return (
+    <div className="App">
+        Hello world
+        <hr/>
+        <Button></Button>
+        <Button></Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App/Component/UI/Button/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Button.css";
+
+export function Button(props){
+    return <button className="Button"> Cliquez moi !!!</button>
+}
+
+//export default Button;
